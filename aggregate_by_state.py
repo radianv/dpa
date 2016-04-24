45c21e39a80599a9144c74364bfd16f28fc23e62
Adrian Vazquez <avazqu83@itam.mx>
2016-04-24 00:23:01 -0500

    version 1.- cambio aggregate_by_state.py

--- aggregate_by_state.py
+++ aggregate_by_state.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     print("Iniciando la tarea en spark")
 
     sqlCtx.read.format('com.databricks.spark.csv')\
-                               .options(header='true', inferSchema='true', delimiter='\t')\
+                               .options(header='true', inferSchema='true', delimiter=',')\
                                .load(sys.argv[1])
                                .registerTempTable('avistamientos')
     sqlCtx.sql("select State as estado, count(*) as avistamientos from avistamientos group by State")\
